4c93bf299bfa911974c040128ff7591d50a820c9
Nayan0406git config --global user.email <nayannikhare83@gmail.com>
2025-08-04 16:40:40 +0530

    add frontend url

--- backend/createSubscription.js
+++ backend/createSubscription.js
@@ -15,7 +15,7 @@ mongoose.connect(process.env.MONGO_DB_URI).then(() => {
 
 const app = express();
 app.use(cors({ 
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173', "https://reiwametta-foundation-frontend.vercel.app/"],
   credentials: true 
 }));
 
