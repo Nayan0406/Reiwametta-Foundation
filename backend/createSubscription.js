ac3f104b875bfc85821c287764ecb34a616a0cec
Nayan0406git config --global user.email <nayannikhare83@gmail.com>
2025-08-04 18:01:11 +0530

    remove

--- backend/createSubscription.js
+++ backend/createSubscription.js
@@ -41,7 +41,6 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'https://reiwametta-foundation-frontend.vercel.app',
-    'https://reiwametta-foundation-frontend.vercel.app/',
   ],
   credentials: true 
 }));
